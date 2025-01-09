6d213f94d81de28e44805c8388463b0541fc7c55
MalejandroC <macarrion44@utpl.edu.ec>
2025-01-09 11:19:17 -0500

    PROBLEMAS

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -16,11 +16,25 @@ public class Problema1 {
      */
     public static void main(String[] args) {
         int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        
+        
         imprimirDatos(informacion);
+        
     }
 
     public static void imprimirDatos(int[][] datos) {
-
+        int i;
+        int x;
+        String cadena = "";
+        for (i=0; i< datos.length; i++) {
+            for (x=0; x< datos[i].length; x++) {
+                if (datos[i][x] % 2 == 0){
+                    cadena = String.format("%s %d ",cadena, datos[i][x]);
+                }
+            }
+        }
+        
+        System.out.printf("%s", cadena);
     }
 
 }
