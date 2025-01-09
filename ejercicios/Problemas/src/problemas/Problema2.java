7fcdef40fef4c72097569b70f4b4b4a53fb66248
MalejandroC <macarrion44@utpl.edu.ec>
2025-01-09 11:30:40 -0500

    PROBLEMAS

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,58 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int datos;
+        System.out.print("Ingrese 1 si desea calcular el area de un cuadrado\n"
+                + "Ingrese 2 si desea calcular el area de un triangulo\n"
+                + "Ingrese 3 si desea calcular el area de un rectangulo´: \n");
+        
+        datos = entrada.nextInt();
+        
+        if (datos == 1) {
+            areaCuadrado();
+        } else {
+            if (datos== 2) {
+                areaTriangulo();
+            } else {
+                if (datos == 3){
+                    areaRectangulo();
+                }
+            }
+        }
     }
     
+    public static void areaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        System.out.print("Ingrese la medida del lado: \n");
+        numero = entrada.nextInt();
+        int formula = numero * numero;
+        
+        System.out.printf("El area del cuadrado es: %d\n", formula);
+    }
+    public static void areaTriangulo() {
+        Scanner entrada = new Scanner(System.in);
+        int altura;
+        int base;
+        System.out.print("Ingrese la medida de la altura: ");
+        altura = entrada.nextInt();
+        System.out.print("Ingrese la medida de la base: ");
+        base = entrada.nextInt();
+        int formula = (altura * base)/2;
+        
+        System.out.printf("El area del cuadrado es: %d\n", formula);
+    }
+    public static void areaRectangulo() {
+        Scanner entrada = new Scanner(System.in);
+        int altura;
+        int base;
+        System.out.print("Ingrese la medida de la altura: ");
+        altura = entrada.nextInt();
+        System.out.print("Ingrese la medida de la base: ");
+        base = entrada.nextInt();
+        int formula = altura * base;
+        
+        System.out.printf("El area del rectangulo es: %d\n", formula);
+    }
 }
