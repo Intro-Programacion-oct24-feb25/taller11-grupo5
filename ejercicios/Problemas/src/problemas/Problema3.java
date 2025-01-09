4162770317ab5e149ace95e2b573f05fe5ac3a87
MalejandroC <macarrion44@utpl.edu.ec>
2025-01-09 11:49:47 -0500

    PROBLEMAS

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,60 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        double nota1;
+        double nota2;
+        double nota3;
+        double nota4;
+
+        System.out.print("Ingrese la nota: ");
+        nota1 = entrada.nextDouble();
+        System.out.print("Ingrese la nota: ");
+        nota2 = entrada.nextDouble();
+        System.out.print("Ingrese la nota: ");
+        nota3 = entrada.nextDouble();
+        System.out.print("Ingrese la nota: ");
+        nota4 = entrada.nextDouble();
+
+        String cadena = notaCualitativa(nota1, nota2, nota3, nota4);
+        String cualitativaMayuscula = mayuscula(cadena);
+
+        System.out.printf("El estudiante con las notas: %.2f;   %.2f;   %.2f;   %.2f "
+                + "tiene un promedio cualitativo: %s\n",
+                nota1,
+                nota2,
+                nota3,
+                nota4,
+                cualitativaMayuscula);
+
+    }
+
+    public static String notaCualitativa(double x, double y, double z, double w) {
+        double promedio;
+        promedio = (w + y + z + w) / 4;
+        String cualitativaa = "";
+        if (promedio >= 0 && promedio <= 5) {
+            cualitativaa = "Regular";
+        } else {
+            if (promedio >= 5.1 && promedio <= 8) {
+                cualitativaa = "Bueno";
+            } else {
+                if (promedio >= 8.1 && promedio <= 9) {
+                    cualitativaa = "Muy bueno";
+                } else {
+                    if (promedio >= 9.1 && promedio <= 10) {
+                        cualitativaa = "Sobresaliente";
+                    }
+                }
+
+            }
+        }
+        return cualitativaa;
     }
     
+    public static String mayuscula (String x) {
+        String may = x.toUpperCase();
+        
+        return may;
+    }
 }
