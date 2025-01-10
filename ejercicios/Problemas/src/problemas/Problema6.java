f883e728a3f121f3d7938c603dcdf7bad0744a4f
MalejandroC <macarrion44@utpl.edu.ec>
2025-01-09 19:55:42 -0500

    PROBLEMAS

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,39 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
+
+    public static void main(String[] args) {
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+
+        double mediaAritmetica = media(informacion);
+        
+        System.out.println("La media aritmecia: "+ mediaAritmetica);
+        
+        double desviacion = desviacionEstandar(informacion, mediaAritmetica);
+        System.out.printf("La desviacion estandar:  %.2f   ", desviacion);
+    }
+
+    public static double media(int[] a) {
+        int suma = 0;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + a[i];
+        }
+
+        double promedio = (double)suma / a.length;
+        return promedio;
+    }
     
+    public static double desviacionEstandar(int []y,double x) {
+        
+        double suma = 0;
+                
+        for (int i = 0; i < y.length; i++) {
+            suma=  suma + Math.pow(y[i]-x ,2);
+            
+        }     
+        
+        double variancia = suma / y.length;
+               
+        return  Math.sqrt(variancia);
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,10 +5,43 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    public static void main(String[] args) {
+        String[] ciudades = ingresarCiudad();
+        numciudades(ciudades);
+    }
+
+    public static String[] ingresarCiudad() {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el número de ciudades:");
+        int numCiudades = leer.nextInt();
+        leer.nextLine();
+        String[] ciudades = new String[numCiudades];
+
+        for (int i = 0; i < numCiudades; i++) {
+            System.out.print("Ingrese la ciudad:" + (i + 1) + " ");
+            ciudades[i] = leer.nextLine();
+        }
+
+        return ciudades;
+
+    }
+
+    public static void numciudades(String[] x) {
+        System.out.println("\nCiudades con 4 o 5 caracteres");
+        for (int i = 0; i < x.length; i++) {
+            String ciudad = x[i];
+            if (ciudad.length() == 4 || ciudad.length() == 5) {
+                System.out.println(ciudad);
+            }
+        }
+
+    }
 }
